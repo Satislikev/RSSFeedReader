faf1f1d1007cc131f12e5512e1a3e5a34061d7a6
Kamyar Aflaki <kamy.aflaki@gmail.com>
2013-05-05 17:48:51 +0200

    Creating New User Class. Modifying Feed and Category.

--- 1DV008_GitHub/src/project/Article.java
+++ 1DV008_GitHub/src/project/Article.java
@@ -1,40 +1,38 @@
 package project;
 
 public class Article {
-	
-		String title;
-		String description;
-		String author;
-		
-		
-		public String getTitle(){
-			return title;
-		}
-		
-		public void setTitle(String title){
-			this.title = title;
-		}
-		
-		public String getDescription(){
-			return description;
-		}
-		
-		public void setDescription(String description){
-			this.description = description;
-		}
-		
-		
-		public String getAuthor(){
-			return author;
-		}
-		
-		public void setAuthor(String author){
-			this.author = author;
-		}
-		
-		
-		public String toString(){
-			return "ArticleClass[title=" + title + ", description=" + description + ", author=" + author+" ]";	
+
+	String title;
+	String description;
+	String author;
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public String toString() {
+		return "ArticleClass[title=" + title + ", description=" + description
+				+ ", author=" + author + " ]";
 
 	}
 
--- 1DV008_GitHub/src/project/UserClass.java
+++ 1DV008_GitHub/src/project/UserClass.java
@@ -9,62 +9,62 @@ public class UserClass {
 	String name;
 	String password;
 	String Categories;
-	
+
 	final List<Category> entries = new ArrayList<Category>();
 	public void Feed(String name, String password){
-		
+
 		this.name = name;
-		
+
 		this.password = password;
 	}
-	
+
 	public List<Category> getCategories(){
 		return entries;
 	}
-		
+
 	public String getName(){
 		return name;
 	}
-	
+
 	public void setName(String name){
 		this.name = name;
 	}
-	
+
 	public String getPassword(){
 		return password;
 	}
-	
+
 	public void setPassword(String password){
 		this.password = password;
-		
+
 	}	
-		
-		 public String toString() {
-			    return "Categories [name=" + name + ", password=" + password + "]";	
-			    
-		 }
-		
-		 
-public void add(String Categories, String title){
-	
+
+	public String toString() {
+		return "Categories [name=" + name + ", password=" + password + "]";	
+
+	}
+
+
+	public void add(String Categories, String title){
+
+	}
+
+
+
+	public static void main(String[] args) {
+
+		// Sort strings - or any other Comparable objects.
+
+		String[] category = {"Categories"};
+		Arrays.sort(category);
+		System.out.println(Arrays.toString(category));
+
+
+
+
+
+	}
+
 }
-
-
-		 
-public static void main(String[] args) {
-				   
-// Sort strings - or any other Comparable objects.
-				   
-String[] category = {"Categories"};
-Arrays.sort(category);
-System.out.println(Arrays.toString(category));
-			    
-			    
-			    
-			    
-		
-	}
-
-	}
 
 
