91a9c57a2ade4fede74e2c1a266e88499c2dd53f
Kamyar Aflaki <kamy.aflaki@gmail.com>
2013-05-22 15:19:41 +0200

    Commiting Article and RWToDB.

--- 1DV008_GitHub/src/project/RWToDatabase.java
+++ 1DV008_GitHub/src/project/RWToDatabase.java
@@ -22,7 +22,7 @@ public class RWToDatabase {
 	private static List<String> categoryList;
 	private static List<String> feedList;
 	private static int categoryID;
-	
+
 	public static boolean nameExists(String name) {
 		startConnection();
 		try {
@@ -40,26 +40,26 @@ public class RWToDatabase {
 		closeConnection();
 		return false;
 	}
-	
+
 	public static boolean passwordMatches(String name, String password) {
 		startConnection();
 		try {
 			preparedStatement = connection.prepareStatement("SELECT * FROM rssDB.user WHERE name = ?;");
 			preparedStatement.setString(1, name);
 			resultSet = preparedStatement.executeQuery();
-//			while (resultSet.next()) {
-				if (resultSet.getString("password").equals(password)) {
-					closeConnection();
-					return true;
-				}
-//			}
+			//			while (resultSet.next()) {
+			if (resultSet.getString("password").equals(password)) {
+				closeConnection();
+				return true;
+			}
+			//			}
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		closeConnection();
 		return false;
 	}
-	
+
 
 	public static int getUserID(String username) {
 		startConnection();
@@ -75,7 +75,7 @@ public class RWToDatabase {
 		closeConnection();
 		return userID;
 	}
-	
+
 	public static String[] getUsersCategoryList(int userID) {
 		startConnection();
 		categoryList = new ArrayList<String>();
@@ -95,7 +95,7 @@ public class RWToDatabase {
 		closeConnection();
 		return result;
 	}
-	
+
 	public static String[] getCategorysFeedList(int userID, int categoryID) {
 		startConnection();
 		feedList = new ArrayList<String>();
@@ -116,7 +116,7 @@ public class RWToDatabase {
 		closeConnection();
 		return result;
 	}
-	
+
 	public static int getCategoryID(String categoryName) {
 		startConnection();
 		categoryID = 0;
@@ -131,27 +131,27 @@ public class RWToDatabase {
 		closeConnection();
 		return categoryID;
 	}
-	
+
 	public static void addCategory(String categoryName, int ownerID) {
-		
+
 	}
-	
+
 	public static void renameCategory(String oldCategoryName, String newCategoryName, int ownerID) {
-		
+
 	}
-	
+
 	public static void removeCategory(String categoryName, int ownerID) {
-		
+
 	}
-	
+
 	public static void addFeed(String title, Path path, Category belongingCategory, User owner) {
-		
+
 	}
-	
+
 	public static void removeFeed(String title, Category belongingCategory, User owner) {
-		
+
 	}
-	
+
 	private static void startConnection() {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
@@ -161,20 +161,20 @@ public class RWToDatabase {
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+//
 	private static void closeConnection() {
 		try {
-            if (resultSet != null)
-            	resultSet.close();
-            if (preparedStatement != null)
-            	preparedStatement.close();
-            if (connection != null)
-            	connection.close();
-        } catch (SQLException e) {
-        	e.printStackTrace();
-        }
+			if (resultSet != null)
+				resultSet.close();
+			if (preparedStatement != null)
+				preparedStatement.close();
+			if (connection != null)
+				connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
